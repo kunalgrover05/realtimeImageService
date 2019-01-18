50dc5d2e8fbeae7060728f532600dbd28d6891d1
Kunal Grover <kunalgrover05@gmail.com>
2019-01-19 01:48:48 +0530

    File manager with diff checker

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,10 @@ const cron = require("node-cron");
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+import { FileManager } from "./files";
 
 import { ImageConverter } from "./convert";
+const fileManager = new FileManager(s3, 'image-realtime', 'converted/');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -30,3 +32,13 @@ app.get('/uploadCallback', function(req, res) {
 io.on('connection', function(socket){
   console.log('a user connected');
 });
+
+cron.schedule("*/10 * * * * *", function() {
+   fileManager.haveFilesChanged()
+      .then(haveChanged => {
+         console.log("Changes in files", haveChanged);
+      })
+   console.log("running a task every minute");
+   io.emit('update', { for: 'everyone' });
+ });
+
---
+++ src/files.ts
@@ -0,0 +1,55 @@
+class S3File {
+    Key: string;
+    ETag: string;
+    Url: string;
+}
+export class FileManager {
+    files: Array<S3File> = new Array();
+    constructor(private s3, private bucket: string, private prefix: string) {
+
+    }
+
+    fetchFileUrls() {
+
+    }
+
+    private fetchFiles(): Promise<Array<S3File>> {
+        return this.s3.listObjectsV2({ Bucket: this.bucket, MaxKeys: 1000, Prefix: this.prefix }).promise()
+            .then(data => {
+                const newFiles: Array<S3File> = data.Contents.map(file => {
+                    return <S3File>{
+                        Key: file.Key,
+                        ETag: file.ETag
+                    }
+                });
+                return newFiles;
+            });
+    }
+
+    private static areEqual(array1: Array<S3File>, array2: Array<S3File>) {
+        if (array1.length !== array2.length) {
+            return false;
+        }
+
+        for (var i=0; i<array1.length; ++i) {
+            if (array1[i].ETag !== array2[i].ETag) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    haveFilesChanged(): Promise<Boolean> {
+        return this.fetchFiles()
+            .then(newFiles => {
+                if (FileManager.areEqual(newFiles, this.files)) {
+                    return false;
+                }
+
+                this.files = newFiles;
+                return true;
+            });
+
+    }
+}
