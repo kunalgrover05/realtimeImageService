2e5514487c7b58b6f8928dca56031bb2bc8f3bd3
Kunal Grover <kunalgrover05@gmail.com>
2019-01-19 03:02:16 +0530

    Logging

--- src/app.ts
+++ src/app.ts
@@ -10,39 +10,55 @@ var io = require('socket.io')(http);
 import { FileManager } from "./files";
 
 import { ImageConverter } from "./convert";
+import { request } from "http";
 const fileManager = new FileManager(s3, 'image-realtime', 'converted/');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(8081, function(){
+http.listen(8081, function () {
    console.log('listening on *:8081');
- });
- 
+});
+
 const imageConverter = new ImageConverter(s3, 'image-realtime', 'converted/');
 
-var client = SNSClient(function(err, message) {
+var auth = {
+   verify: false
+};
+var client = SNSClient(auth, function (err, message) {
    console.log(message);
-
-   imageConverter.convertFile('flutter-2.jpg');
+   // console.log(message);
+   console.log("Converting");
+   imageConverter.convertFile('flutter-2.jpg').then(data => {
+      console.log("Conversion completed");
+   }).catch(err => {
+      console.log("ERROR");
+      console.log(err);
+   });
 });
 
-app.post('/uploadCallback', function(req, resp){
-   return client(req, resp);   
+app.post('/uploadCallback', function (req, resp) {
+   //Get the message type header.
+   const messagetype = req.get("x-amz-sns-message-type");
+   console.log(messagetype);
+   console.log("Upload callback");
+   console.log(req.body);
+   if (messagetype === "Notification") {
+   } else if (messagetype === "SubscriptionConfirmation") {
+      console.log(req.body);
+   }
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function (socket) {
+   console.log('a user connected');
 });
 
-cron.schedule("*/10 * * * * *", function() {
+cron.schedule("*/100 * * * * *", function () {
    fileManager.haveFilesChanged()
       .then(haveChanged => {
-         console.log("Changes in files", haveChanged);
       })
-   console.log("running a task every minute");
    io.emit('update', { for: 'everyone' });
- });
+});
 
 
