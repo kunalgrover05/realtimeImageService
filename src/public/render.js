e9e18c27e8f53c1a9c6cff147074526882865dba
Kunal Grover <kunalgrover05@gmail.com>
2019-01-19 17:55:05 +0530

    Cleaner rendering, background

--- src/public/render.js
+++ src/public/render.js
@@ -7,7 +7,6 @@ var app = new Vue({
     data: {
         files: []
     },
-    template: '<div  style="flex-flow: wrap; flex-direction: row; align-items: center; display: flex"><img v-for="file in files" :src="file.Url" style="max-width: 250px" /></div>'
 })
 
 
